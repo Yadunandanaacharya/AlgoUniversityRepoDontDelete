50c6ff1cf2985eee0efae319ff1dba926f98775f
yadu <yadunandana@vegam.co>
2022-11-06 12:42:17 +0530

    p25 commit

---
+++ DSAProblemsOnly/src/A4SearchSort1/p25DirectBinarySeach.java
@@ -0,0 +1,16 @@
+package A4SearchSort1;
+
+/*
+DANGER: TRY TO CODE EVERY WORD OF CODE BY YOURSELF DON'T CLICK ON INTELLISENCE
+IN ONLINE ASSESSMENT YOU WON'T GET ANY INTELLISENSE EXAMPLE IN LEETCODE YOU WON'T GET ANY OF THOSE
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class p25DirectBinarySeach {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
